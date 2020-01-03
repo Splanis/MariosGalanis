9d51ef35bef79da1aa60aa8edeba2228ad34a2d5
Marios G <splanis5@gmail.com>
2020-01-03 17:53:33 +0200

    active links at navbar added

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {
+    var path = location.href.split("/").pop();
+    if (path == ''){
+        path = '#home';
+    }
+    var target = $('nav a[href="'+path+'"]');
+    target.addClass('active');
+});
+
+$(document).on('click', 'ul li a', function(){
+    $(this).addClass('active')
+    $('ul li a').not(this).removeClass('active')
+});
+
