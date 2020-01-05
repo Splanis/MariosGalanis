03ec120aef667e71b1790d2ee16c58e6e2896a7e
Marios G <splanis5@gmail.com>
2020-01-05 03:05:54 +0200

    footer fixed

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-$(function() {
+$(function () {
     var path = location.href.split("/").pop();
-    if (path == ''){
-        path = '#home';
+
+    if (path == "" || path == "index.html") {
+        path = "#home";
     }
-    var target = $('nav a[href="'+path+'"]');
-    target.addClass('active');
+    var target = $('nav a[href="' + path + '"]');
+    target.addClass("active");
 });
 
-$(document).on('click', 'ul li a', function(){
-    $(this).addClass('active')
-    $('ul li a').not(this).removeClass('active')
+$(document).on("click", "ul li a", function () {
+    $(this).addClass("active");
+    $("ul li a")
+        .not(this)
+        .removeClass("active");
 });
-
