caa30d7e96f381cd550f36b90f3dd0d384a1a56e
Marios G <splanis5@gmail.com>
2020-02-06 22:03:59 +0200

    code formatted

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 AOS.init();
 
 // Active CSS NavBar Links
-$(function () {
+$(function() {
     var path = location.href.split("/").pop();
 
     if (path == "" || path == "index.html") {
@@ -12,7 +12,7 @@ $(function () {
     target.addClass("active");
 });
 
-$(document).on("click", "ul li a", function () {
+$(document).on("click", "ul li a", function() {
     $(this).addClass("active");
     $("ul li a")
         .not(this)
@@ -20,20 +20,19 @@ $(document).on("click", "ul li a", function () {
 });
 
 // Particles.js Background
-particlesJS.load('particles-js', 'assets/particlesjs-config.json', function () {
-    console.log('callback - particles.js config loaded');
+particlesJS.load("particles-js", "assets/particlesjs-config.json", function() {
+    console.log("callback - particles.js config loaded");
 });
 
 // Contact Form
-$(function () {
-    $('.contact-submit-btn').click(function (event) {
-
-        var email = $('.email').val()
-        var name = $('.name').val()
-        var subject = $('.subject').val()
-        var message = $('.message').val()
-        var statusElement = $('.errors')
-        statusElement.empty()
+$(function() {
+    $(".contact-submit-btn").click(function(event) {
+        var email = $(".email").val();
+        var name = $(".name").val();
+        var subject = $(".subject").val();
+        var message = $(".message").val();
+        var statusElement = $(".errors");
+        statusElement.empty();
 
         function validateEmail(email) {
             var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -41,58 +40,62 @@ $(function () {
         }
 
         if (!validateEmail(email)) {
-            statusElement.append('<div>-Please enter a valid Email Address.</div>')
-            event.preventDefault()
+            statusElement.append("<div>-Please enter a valid Email Address.</div>");
+            event.preventDefault();
         }
 
         if (subject.length <= 0) {
-            statusElement.append('<div>-Please give a message subject.</div>')
-            event.preventDefault()
+            statusElement.append("<div>-Please give a message subject.</div>");
+            event.preventDefault();
         }
 
         if (name.length <= 0) {
-            statusElement.append('<div>-Please enter your name.</div>')
-            event.preventDefault()
+            statusElement.append("<div>-Please enter your name.</div>");
+            event.preventDefault();
         }
 
         if (message.length <= 0) {
-            statusElement.append('<div>-Please enter a message.</div>')
-            event.preventDefault()
+            statusElement.append("<div>-Please enter a message.</div>");
+            event.preventDefault();
         }
-    })
+    });
 });
 
 // Smooth Scrolling
-$(document).ready(function () {
-    $("a").on('click', function (event) {
+$(document).ready(function() {
+    $("a").on("click", function(event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-                window.location.hash = hash;
-            });
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top
+                },
+                800,
+                function() {
+                    window.location.hash = hash;
+                }
+            );
         }
     });
 });
 
 // Open and close SideNav
-$(".open-sidenav-btn").click(function () {
-    $('.side-nav').css("margin-left", "0");
-    $('nav').css("margin-top", "-60px");
+$(".open-sidenav-btn").click(function() {
+    $(".side-nav").css("margin-left", "0");
+    $("nav").css("margin-top", "-60px");
 });
 
-$(".close-btn, .side-nav-links a").click(function () {
-    $('.side-nav').css("margin-left", "-300px");
-    $('nav').css("margin-top", "0");
+$(".close-btn, .side-nav-links a").click(function() {
+    $(".side-nav").css("margin-left", "-300px");
+    $("nav").css("margin-top", "0");
 });
 
 $(document).click(function(e) {
     var target = e.target;
-    if (!$(target).is('.side-nav') && $('.side-nav').css('margin-left') == '0px') {
-        $('.side-nav').css("margin-left", "-300px");
-        $('nav').css("margin-top", "0");
+    if (!$(target).is(".side-nav") && $(".side-nav").css("margin-left") == "0px") {
+        $(".side-nav").css("margin-left", "-300px");
+        $("nav").css("margin-top", "0");
     }
 });
